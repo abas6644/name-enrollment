5c0a5511fad937a18628f8b803a21f1b4c344186
Abasiubong <ubongabasi09@gmail.com>
2021-12-09 10:32:46 -0800

    commit

---
+++ youngie.py
@@ -0,0 +1,9 @@
+golden=input("enter your name: ")
+names_of_persons =["john","grace","longman","precious","sammy"]
+if golden==names_of_persons:
+    print("you are welcome")
+else:
+    print("please check back next time")
+    
+
+
